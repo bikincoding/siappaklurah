b532dc97aa900b7bc5870e20e5e651c5713959ee
bikincoding <91186943+bikincoding@users.noreply.github.com>
2024-09-26 17:56:48 +0800

    logo kerobokan kaja

--- resources/views/layouts/app_admin.blade.php
+++ resources/views/layouts/app_admin.blade.php
@@ -16,7 +16,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet">
-
+    <link rel="icon" href="{{ asset('image/logo kerobokan kaja.jpg) }}" type="image/jpg">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
